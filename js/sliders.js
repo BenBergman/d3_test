3265500b70a5e126b62bfb7db5c1b94812e3dd56
Benjamin Bergman <ben@benbergman.ca>
2016-08-17 23:52:56 -0500

    Switch slider balancing to a method that allows objects instead of just lists

--- js/sliders.js
+++ js/sliders.js
@@ -7,29 +7,29 @@ if (typeof module != "undefined") {
 
 function rebalance(total, slider_values, changed_index) {
     var unchanged_total = 0;
-    var weight_of_unchanged = [];
 
-    for (var i = 0; i < slider_values.length; i++) {
-        if (i != changed_index) {
-            unchanged_total += slider_values[i];
+    for (var k in slider_values) {
+        if (k != changed_index) {
+            unchanged_total += slider_values[k];
         }
     }
 
-    for (var i = 0; i < slider_values.length; i++) {
+    var weight_of_unchanged = {};
+    for (var k in slider_values) {
         var weight = 0;
         if (unchanged_total == 0) {
-            weight = 1 / (slider_values.length - 1);
+            weight = 1 / (Object.keys(slider_values).length - 1);
         } else {
-            weight = slider_values[i] / unchanged_total;
+            weight = slider_values[k] / unchanged_total;
         }
-        weight_of_unchanged[i] = weight;
+        weight_of_unchanged[k] = weight;
     }
 
     var new_weight = slider_values;
 
-    for (var i = 0; i < slider_values.length; i++) {
-        if (i != changed_index) {
-            new_weight[i] = (total - slider_values[changed_index]) * weight_of_unchanged[i];
+    for (var k in slider_values) {
+        if (k != changed_index) {
+            new_weight[k] = (total - slider_values[changed_index]) * weight_of_unchanged[k];
         }
     }
 
--- js/sliders.test.js
+++ js/sliders.test.js
@@ -3,6 +3,16 @@ import test from 'ava';
 var sliders = require('./sliders.js');
 
 
-test('gree', t => {
-    t.deepEqual(sliders.rebalance(10, [7, 0, 0, 0], 0), [7, 1, 1, 1]);
+test('all untouched sliders at 0 means split available points evenly', t => {
+    t.deepEqual(sliders.rebalance(10, {a: 7, b: 0, c: 0, d: 0}, 'a'), {a: 7, b: 1, c: 1, d: 1});
+})
+
+
+test('all untouched sliders at equal value means new available amounts are distributed equally', t => {
+    t.deepEqual(sliders.rebalance(10, [4, 3, 3, 3], 0), [4, 2, 2, 2]);
+})
+
+
+test('uneven sliders means new available amounts are distributed proportionally', t => {
+    t.deepEqual(sliders.rebalance(10, [0, 1, 2, 1], 0), [0, 2.5, 5, 2.5]);
 })
